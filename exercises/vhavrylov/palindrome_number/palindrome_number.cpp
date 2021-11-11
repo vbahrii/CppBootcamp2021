84418302558367f476c7a6d2fc4f66f41b2ebb13
Volodymyr Havrylov <khaemn@gmail.com>
2021-11-11 19:29:48 +0200

    Refactors the test case constants

--- exercises/vhavrylov/palindrome_number/palindrome_number.cpp
+++ exercises/vhavrylov/palindrome_number/palindrome_number.cpp
@@ -10,15 +10,6 @@
 using std::cout;
 using std::endl;
 
-// Having two separate arrays is soo inconvenient, it is easy to mismatch their
-// sizes and it looks ugly when there are a lot of test cases :(
-static int tests[] = {
-    121, -121,       1,           -0,  -1,  1234, 12344321, 124545421, 95100159,
-    12,  2147483647, -2147483648, 951, 159, 2242, 2422,     2442};
-static bool answers[] = {true,  false, true,  true,  false, false,
-                         true,  true,  true,  false, false, false,
-                         false, false, false, false, true};
-
 bool is_palindrome_number(int number) {
   if (number < 0) {
     return false;
@@ -59,6 +50,16 @@ bool is_palindrome_number(int number) {
 }
 
 int main() {
+  // Having two separate arrays is soo inconvenient, it is easy to mismatch
+  // their sizes and it looks ugly when there are a lot of test cases :(
+  static const int tests[] = {
+      121,      -121,      1,        -0,   -1,         1234,
+      12344321, 124545421, 95100159, 12,   2147483647, -2147483648,
+      951,      159,       2242,     2422, 2442};
+  static const bool answers[] = {true,  false, true,  true,  false, false,
+                                 true,  true,  true,  false, false, false,
+                                 false, false, false, false, true};
+
   const int total_tests = (sizeof(tests) / sizeof(*tests));
   const int total_answers = (sizeof(answers) / sizeof(*answers));
   if (total_tests != total_answers) {
